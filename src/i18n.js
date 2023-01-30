f2d3e60e0509ec4a18454bb012968eab38cdbdc7
aarmigl <aarmigl@arte-consultores.com>
2023-01-30 16:10:31 +0000

    EDATOS-3876 Desarrollo del sistema de producción de visualizaciones (e-Gráficos)
    > Prueba definir ruta traducciones

--- src/i18n.js
+++ src/i18n.js
@@ -27,7 +27,7 @@ i18n
     },
     backend: {
       // for all available options read the backend's repository readme file
-      loadPath: '/locales/{{lng}}/{{ns}}.json'
+      loadPath: process.env.PUBLIC_URL +'/locales/{{lng}}/{{ns}}.json'
     }
   });
 
