557c7d025c1e436aafcfe8b350d64f09f7c622ad
ANTONIO PEREZ SANTANA <apersan@arte-consultores.com>
2023-03-02 15:28:09 +0000

    EDATOS-3876 Desarrollo del sistema de producción de visualizaciones (e-Gráficos)
    - Añadimos unmount a widget

--- src/widget/index.js
+++ src/widget/index.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
-import EDatosGraph from './components/echart';
+import EDatosGraph from './components/echart'
 
-let init = null;
+let init = null
 
 export default {
-   config: (config) => {
-      init = config;
-   },
-   widgets: {
-      egraph: {
-         new: () => {
-            return {
-              render: () => {
-                  ReactDOM.render(
-                      <React.StrictMode>
-                          <EDatosGraph options={init.options} renderer={init.renderer}/>
-                      </React.StrictMode>, document.querySelector(init.selector));
-              }
-            }
-          }
-      }
-   }
+  config: (config) => {
+    init = config
+  },
+  widgets: {
+    egraph: {
+      new: () => {
+        return {
+          render: () => {
+            ReactDOM.render(
+              <React.StrictMode>
+                <EDatosGraph options={init.options} renderer={init.renderer} />
+              </React.StrictMode>,
+              document.querySelector(init.selector)
+            )
+          },
+          unmount: () => {
+              ReactDOM.unmountComponentAtNode(document.querySelector(init.selector));
+          },
+        }
+      },
+    },
+  },
 }
-
