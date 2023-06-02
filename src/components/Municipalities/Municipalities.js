8e1ddb33f743d3166d69897dd0062b2812886c61
ANTONIO PEREZ SANTANA <apersan@arte-consultores.com>
2023-06-02 13:15:49 +0100

    EDATOS-3876 Desarrollo del sistema de producción de visualizaciones (e-Gráficos)
    - Parametrizamos url de datos de localización de municipios

--- src/components/Municipalities/Municipalities.js
+++ src/components/Municipalities/Municipalities.js
@@ -3,15 +3,14 @@ import { Typeahead } from 'react-bootstrap-typeahead'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Papa from 'papaparse'
+import applicationConfig from '../../application.json'
 
 function Municipalities(props) {
   const { t } = useTranslation(['translation'])
   const [municipalities, setMunicipalities] = useState([])
   useEffect(() => {
     axios
-      .get(
-        'https://datos.canarias.es/catalogos/estadisticas/dataset/6dd8baf4-14f4-43a3-88b2-984d034c965c/resource/74237bc8-1e5f-43f1-9f6b-24ef9e073903/download/municipios_desde2007_20170101.csv'
-      )
+      .get(applicationConfig['municipalities']['endpoint'])
       .then((response) => {
         Papa.parse(response.data, {
           header: true,
