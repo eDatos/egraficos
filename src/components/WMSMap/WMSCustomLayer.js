938539bd2e03305bd380ed9cbb52ad33ff7a5b36
ANTONIO PEREZ SANTANA <apersan@arte-consultores.com>
2023-06-05 13:11:34 +0100

    EDATOS-3876 Desarrollo del sistema de producción de visualizaciones (e-Gráficos)
    - No mostramos popup de información si esta no existe

--- src/components/WMSMap/WMSCustomLayer.js
+++ src/components/WMSMap/WMSCustomLayer.js
@@ -6,19 +6,18 @@ const WMSCustomLayer = ({ layer, url }) => {
   const context = useLeafletContext()
   const layerRef = useRef()
   const layerName = layer['Name']
+  var MySource = WMS.Source.extend({
+    showFeatureInfo: function (latlng, info) {
+      if (!this._map || !info) {
+        return
+      }
+      this._map.openPopup(info, latlng)
+    },
+  })
 
   useEffect(() => {
     const selectedStyles = layer.StyleSelected?.styleName ?? []
     const container = context.layerContainer || context.map
-
-    var MySource = WMS.Source.extend({
-      showFeatureInfo: function (latlng, info) {
-        if (!this._map || !info) {
-          return
-        }
-        this._map.openPopup(info, latlng)
-      },
-    })
 
     const source = new MySource(url, {
       format: 'image/png',
