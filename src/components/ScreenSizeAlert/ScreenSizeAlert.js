02e238fbbd52da12ee9a5b76dca4441b50dd2ebe
ANTONIO PEREZ SANTANA <apersan@arte-consultores.com>
2023-06-14 16:18:36 +0100

    EDATOS-3876 Desarrollo del sistema de producción de visualizaciones (e-Gráficos)
    - Actualizamos alerta de tamaño de pantalla usado

--- src/components/ScreenSizeAlert/ScreenSizeAlert.js
+++ src/components/ScreenSizeAlert/ScreenSizeAlert.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import useWindowSize from '../../hooks/useWindowSize';
 import { Modal, Button } from 'react-bootstrap';
 import { BsEnvelopeFill } from 'react-icons/bs';
+import { useTranslation } from 'react-i18next';
 
 // import styles from './ScreenSizeAlert.module.scss'
 
@@ -9,6 +10,7 @@ function ScreenSizeAlert() {
   const size = useWindowSize();
   const [showModal, setShowModal] = useState(size.width < 992);
   const [modalWasClosed, setModalWasClosed] = useState(false);
+  const {t} = useTranslation(['translation']);
 
   const handleClose = () => {
     setShowModal(false);
@@ -32,31 +34,30 @@ function ScreenSizeAlert() {
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header closeButton>
+      <Modal.Header>
         <Modal.Title as="h5">
           <span role="img" aria-label="Party icon">
             🎉
           </span>{' '}
-          Welcome to the new RAWGraphs!
+          {t('global.screenSizeAlert.header')}
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <p className="big">
-          RAWGraphs 2.0 is designed for {size.width >= 768 ? 'slightly ' : ' '}
-          bigger screens!
+        {t('global.screenSizeAlert.body.header')}
         </p>
         <p>
-          Resize your browser window or{' '}
+          {t('global.screenSizeAlert.body.text.1')}
           <a href="mailto:?subject=Visit+RAWGraphs+2.0&body=Hello%21%0D%0APlease+do+not+forget+to+take+a+look+at+the+new+version+of+RAWGraphs%21%0D%0A%0D%0AVisit%3A+https%3A%2F%2Fdev.rawgraphs.io%2F%0D%0A%0D%0ASee+you+later%2C%0D%0AThe+RAWGraphs+Team">
-            <BsEnvelopeFill /> send yourself a reminder
-          </a>{' '}
-          to come back at a better time.
+            <BsEnvelopeFill /> {t('global.screenSizeAlert.body.text.2')}
+          </a>
+          {t('global.screenSizeAlert.body.text.3')}
         </p>
-        <p>Touch devices are not fully supported yet.</p>
+        <p>{t('global.screenSizeAlert.body.footer')}</p>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="primary" onClick={handleClose}>
-          Got it!
+        {t('global.screenSizeAlert.gotIt')}
         </Button>
       </Modal.Footer>
     </Modal>
